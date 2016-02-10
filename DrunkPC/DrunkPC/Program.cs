14d5653e65d74df6b2d2136cffd0842fbf34bf7e
Daniel Marifjeren <daniel@epicdaniel.com>
2016-02-10 13:40:11 -0500

    adding all files

---
+++ DrunkPC/DrunkPC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrunkPC
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry Point For Prank Aapp
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
