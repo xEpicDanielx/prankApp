464c52c6e93ac399b4de626066f7f84040b626dd
Daniel Marifjeren <daniel@epicdaniel.com>
2016-02-10 13:56:12 -0500

    threading example

--- DrunkPC/DrunkPC/Program.cs
+++ DrunkPC/DrunkPC/Program.cs
@@ -4,16 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+
+/// <summary>
+/// Drunk PC 
+/// 
+/// Just to mess with everyone
+/// 
+/// Learning threads
+/// forms
+/// and all that good stuff
+/// as well ad hidden application
+/// 
+/// </summary>
 namespace DrunkPC
 {
     class Program
     {
+        //Underscore indicates global
+        public static Random _random = new Random();
+
         /// <summary>
         /// Entry Point For Prank App
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("DRUNK PC");
+
+            Thread sampleThread1 = new Thread(new ThreadStart(SampleThread1));
+            sampleThread1.Start();
+
+            Thread sampleThread2 = new Thread(new ThreadStart(SampleThread2));
+            sampleThread2.Start();
+
+            Console.ReadKey();
+            sampleThread1.Abort();
+            sampleThread2.Abort();
+
+        }
+        /// <summary>
+        /// This is a sample thread
+        /// </summary>
+        static void SampleThread1() {
+            while (true) {
+                Console.WriteLine("HELLO THREAD!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void SampleThread2()
+        {
+            while (true)
+            {
+                Console.WriteLine("HELLO THREAD2!");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
